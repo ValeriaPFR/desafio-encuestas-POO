6150820c5e031b329434547a4aea866d0dae9eb0
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-04-18 10:08:49 -0400

    Add alternativas.py file

---
+++ alternativas.py
@@ -0,0 +1,10 @@
+#definicion de clase 'Alternativas'
+class Alternativas:
+    #constructor 
+    def __init__(self, contenido, ayuda):
+        self.contenido = None
+        self.ayuda = None
+    #mostrar alternativas
+    def mostrar_alternativa(self, ):
+        pass
+
