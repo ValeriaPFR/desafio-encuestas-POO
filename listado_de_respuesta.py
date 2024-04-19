3fa8724badc0ef10bed725de1c366c1a98ba6ad5
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-04-19 10:20:01 -0400

    Modify listado_de_respuesta.py file, replacing plural with singular in classes, methods and documentation

---
+++ listado_de_respuesta.py
@@ -0,0 +1,18 @@
+class ListadoDeRespuesta:
+    """
+Una clase para manejar una lista de respuestas, asociadas un correo electronico especifico.
+
+Args:
+- correo (str): el correo electronico asiciado a las respuestas
+- respuestas (list): la lista de respuestas
+
+Returns:
+None
+"""
+    def __init__(self, elf, correo:str, respuesta:list):
+        self.__lista_respuesta = respuesta
+        self.__correo = correo 
+    # método para ingresar las respuestaas en una lista vacia
+    def respuesta_append(self, ):
+        pass
+
