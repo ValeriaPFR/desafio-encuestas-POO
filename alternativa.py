8a8764b065626a03175c616895520e6cff20afa2
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-04-19 10:24:06 -0400

    Modify alternativa.py file, replacing plural with singular in classes, methods and documentation

---
+++ alternativa.py
@@ -0,0 +1,9 @@
+#definicion de clase 'Alternativas'
+class Alternativa:
+    #constructor 
+    def __init__(self, contenido, ayuda):
+        self.contenido = None
+        self.ayuda = None
+    #mostrar alternativas
+    def mostrar_alternativa(self, ):
+        pass
