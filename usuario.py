a758825a216b5137101a7d6d821e13b06b87f0c0
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-04-19 10:25:50 -0400

    Modify usuario.py file, replacing plural with singular in classes, methods and documentation

---
+++ usuario.py
@@ -0,0 +1,19 @@
+class Usuario:
+    """
+Una clase para representar los datos de los usuarios
+
+Args:
+- self
+
+Returns:
+None
+"""
+
+    def __init__(self, nombre:str,edad:int, correo:str, region:int):
+        self.nombre = None
+        self.edad = None
+        self.correo = None
+        self.region = None
+    def contestar_encuesta(self, ):
+        pass
+
